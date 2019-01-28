7fb73bb720b013e269bcb025b88271bc5ddc8c0a
CristinaBizoi <44783856+CristinaBizoi@users.noreply.github.com>
2019-01-28 21:00:45 +0200

    Add files via upload
    add index,php

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset = "UTF-8">
+    <meta name = "viewpoint" content = "width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tutorial HTML CSS</title>
+    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,700" rel="stylesheet">  
+</head>
+<body>
+   <div class="header">
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <span class="logo-bold"> Drone</span><span class="logo-light">Zone</span> 
+            </div>
+            <div class="meniu">
+                <ul>
+                    <li><a href="#" class="with-style">Home</a></li>
+                    <li><a href="#" class="with-style">About</a></li>
+                    <li><a href="#" class="with-style">Pricing</a></li>
+                    <li><a href="#" class="with-style">Our drones</a></li>
+                    <li><a href="#" class="with-style">Our relations</a></li>
+                    <li><a href="#" class="with-border" >Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class= "slogan">
+            <h1>Film your event with us!</h1>
+            <p>Pellentesque eget nunc sit amet urna ullamcorper fermentum et eu leo.Nunc vel nibh tempor, pharetra lectus congue, luctus orci.</p>
+            <div class="slogan-buttons">
+                <a href="a" class="right-button">Ask for price</a> 
+                <a href="a" class="watch-button"><i class="fa fa-play"></i>Watch videos</a>     
+            </div>
+        </div>
+    </div>
+   </div>
+   <div class="section-2">
+   <div class="section-2-items">
+       <div class="container">
+            <div class="row">
+                <div class="col-25">
+                    <div class="heading-img">
+                        <img src="./public/imgs/drone.gif" alt="From air">
+                    </div>
+                    <h3 class="gradient"> From air </h3>
+                    <p> llentesque eget nunc sit amet urna ullamcorper fermentum et eu leo. </p>
+                </div>
+                <div class="col-25">
+                    <div class="heading-img">
+                        <img src="./public/imgs/drone-1.gif" alt="Best drones ">
+                    </div>
+                    <h3 class="gradient"> Best drones </h3>
+                    <p> llentesque eget nunc sit amet urna ullamcorper fermentum et eu leo. </p>
+                </div>
+                <div class="col-25">
+                    <div class="heading-img">
+                        <img src="./public/imgs/control.gif" alt="Speed">
+                    </div>
+                    <h3 class="gradient"> Speed </h3>
+                    <p> llentesque eget nunc sit amet urna ullamcorper fermentum et eu leo.</p>
+                </div>
+                <div class="col-25">
+                    <div class="heading-img">    
+                        <img src="./public/imgs/propeller.gif" alt="Long range">
+                    </div>
+                    <h3 class="gradient"> Long range </h3>
+                    <p> llentesque eget nunc sit amet urna ullamcorper fermentum et eu leo.</p>
+                </div>
+            </div>
+            <div class="row text-centered">
+                <a href="#" class="button-blue">Ask for price </a>
+            </div>
+       </div>
+   </div>
+   </div>
+   <div class="section-3">
+   <div class="container">
+        <div class="section-3-items"> 
+            <h2 class="gradient"> Nature from the air </h2>
+            <p class="descriere-sectiune-3"> Mauris consequat libero metus, nec ultricies sem efficitur quis. Integer bibendum eget metus ac accumsan. Integer sit amet lacus egestas, semper est quis, viverra ex. </p>
+       <div class="col">
+        <div class="col-50">
+                <div class="top">
+                <span class="number"> 1 </span>
+                    <p class="descriereitem">Pellentesque eget nunc sit amet urna ullamcorper fermentum et eu leo. Nunc vel nibh tempor, pharetra lectus congue, luctus orci.</p>
+                </div>
+                <div class="top">
+                <span class="number"> 2 </span>
+                    <p class="descriereitem">Mauris consequat libero metus, nec ultricies sem efficitur quis. Integer bibendum eget metus ac accumsan.</p>
+                </div>
+                <div class="top">
+                <span class="number"> 3 </span>
+                    <p class="descriereitem">nteger sit amet lacus egestas, semper est quis, viverra ex. Mauris consequat libero metus, nec ultricies sem efficitur quis.</p>
+                </div>
+        </div>
+        <div class="col-50 right">
+                <div class="top">
+                <span class="number"> 4 </span>
+                    <p class="descriereitem"> Pellentesque eget nunc sit amet urna ullamcorper fermentum et eu leo. Nunc vel nibh tempor, pharetra lectus congue, luctus orci.</p>
+                </div>
+                <div class="top">
+                <span class="number"> 5 </span>
+                    <p class="descriereitem">Mauris consequat libero metus, nec ultricies sem efficitur quis. Integer bibendum eget metus ac accumsan.</p>
+                </div>
+        </div>
+        </div>
+       </div>
+    </div>
+   </div>
+   <div class="section-4">
+   <div class="container">
+        <div class="section-4-items"> 
+            <h2 class="gradient titlu"> Nature from the air </h2>
+            <p class="descriere"> Mauris consequat libero metus, nec ultricies sem efficitur quis. Integer bibendum eget metus ac accumsan. Integer sit amet lacus egestas, semper est quis, viverra ex. </p>
+            <div class="row">
+                <a href="#" class="button-blue">Ask for price </a>
+            </div>
+        </div>
+   </div>
+   <div class="section-5">
+   <div class="container">
+   <div class="section-5-items"> 
+            <h2 class="gradient titlu"> City of the air </h2>
+            <p class="descriere"> Mauris consequat libero metus, nec ultricies sem efficitur quis. Integer bibendum eget metus ac accumsan. Integer sit amet lacus egestas, semper est quis, viverra ex. </p>
+            <div class="row ">
+                <a href="#" class="button-blue">Ask for price </a>
+            </div>
+        </div>
+    </div>
+    </div>
+   </div>
+   <div class="section-6">
+   <div class="section-6-items"> 
+       <div class="container">
+            <h2 class="titlu"> Sign up for our newsletter </h2>
+            <form action="#" method="post" class="formular">
+            <input type="email" placeholder="Email adress">
+            <input type="button" class="right-button send-button" value="Send">
+            </form>
+
+    </div>
+    </div>
+   </div>
+   <div class="section-7">
+   </div>
+</body>
+</html>
